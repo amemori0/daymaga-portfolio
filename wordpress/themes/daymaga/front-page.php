7ced3e13889614144cce050c5e8b3a1b0c2c84df
Amemori <vokey5258fn@gmail.com>
2026-06-26 19:02:38 +0900

    feat(top-new): 新着記事セクションおよび共通UIコンポーネントの実装
    【セクション実装】
    - p-top-new: 新着記事セクションのマークアップおよび基本レイアウトの構築
    - -webkit-line-clampを使用した記事タイトルの2行制限（三点リーダー表示）により、長文テキストによるレイアウト崩れを防止
    
    【汎用コンポーネント】
    - c-section-title: サイト全体で再利用可能なセクションタイトルを定義し、保守性を向上
    - c-button-more: カスタムプロパティとcalc()を用い、ボーダー幅を含めてpaddingを自動計算する堅牢なボタンを実装

--- wordpress/themes/daymaga/front-page.php
+++ wordpress/themes/daymaga/front-page.php
@@ -2,7 +2,7 @@
 
 <main>
   <?php get_template_part("template-parts/front-page/section-mv"); ?>
-  <?php get_template_part("template-parts/front-page/section-sample"); ?>
+  <?php get_template_part("template-parts/front-page/section-new"); ?>
 </main>
 
 <?php get_footer(); ?>
--- wordpress/themes/daymaga/template-parts/front-page/section-new.php
+++ wordpress/themes/daymaga/template-parts/front-page/section-new.php
@@ -1,81 +1,15 @@
-<?php
-/**
- * フロントページ: MV セクション
- */
-?>
-<section class="p-mv">
-  <div class="p-mv__inner">
-
-    <h2 class="u-sr-only">ピックアップ記事</h2>
-    
-    <div class="p-mv__swiper-container" role="region" aria-label="ピックアップ記事スライダー">
-      <div class="swiper p-mv__swiper">
-        <div class="swiper-wrapper p-mv__swiper-wrapper" role="list">
-
-          <div class="swiper-slide p-mv__swiper-slide" role="listitem">
-            <article class="c-card" aria-labelledby="card-title-1" data-status="large">
-              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link" tabindex="-1">
-                
-                <div class="c-card__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304">
-                </div>
-                
-                <div class="c-card__body">
-                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
-                  
-                  <h3 class="c-card__title" id="card-title-1">
-                     <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
-                </h3>
-                  
-                  <div class="c-card__category">
-                    <span class="c-category-label">TIPS</span>
-                  </div>
-                  
-                  <ul class="p-mv__card-tags" aria-label="記事のタグ">
-                    <li class="c-tag">#コンサルファーム</li>
-                    <li class="c-tag">#戦略</li>
-                    <li class="c-tag">#その他</li>
-                    <li class="c-tag">#その他</li>
-                    <li class="c-tag">#その他</li>
-                  </ul>
-                </div>
-                
-              </a>
-            </article>
-          </div>
-
-          <div class="swiper-slide p-mv__swiper-slide" role="listitem">
-            <article class="c-card" aria-labelledby="card-title-1" data-status="large">
-              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link" tabindex="-1">
-                
-                <div class="c-card__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304" loading="lazy" decoding="async">
-                </div>
-                
-                <div class="c-card__body">
-                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
-                
-                  <h3 class="c-card__title" id="card-title-1">
-                   <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
-                   </h3>
-                  
-                  <div class="c-card__category">
-                    <span class="c-category-label">TIPS</span>
-                  </div>
-                  
-                  <ul class="p-mv__card-tags" aria-label="記事のタグ">
-                    <li class="c-tag">#コンサルファーム</li>
-                    <li class="c-tag">#戦略</li>
-                    <li class="c-tag">#その他</li>
-                  </ul>
-                </div>
-                
-              </a>
-            </article>
-          </div>
-
-          <div class="swiper-slide p-mv__swiper-slide" role="listitem">
-            <article class="c-card" aria-labelledby="card-title-1" data-status="large">
+<section class="p-top-new">
+    <div class="p-top-new__inner">
+        <h2 class="c-section-title" data-align="center">
+            <svg aria-hidden="true" focusable="false" width="72" height="38" viewBox="0 0 72 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M42.9056 38C30.7853 26.529 -4.20111 4.86719 0.417236 1.75967C13.3482 -6.94112 27.3432 18.4447 42.9056 38Z" fill="#135097"/>
+                <path d="M34.0415 36.01C43.1022 27.4348 75.5692 16.9946 71.6778 14.3764C59.465 6.15945 45.6753 21.3913 34.0415 36.01Z" fill="#C93A3A"/>
+            </svg>
+            <span>新着記事</span>
+        </h2>
+        <ul class="p-top-new__list">
+            <li class="p-top-new__item">
+         <article class="c-card" aria-labelledby="card-title-1">
               <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link" tabindex="-1">
                 
                 <div class="c-card__img">
@@ -96,26 +30,75 @@
                   <ul class="p-mv__card-tags" aria-label="記事のタグ">
                     <li class="c-tag">#コンサルファーム</li>
                     <li class="c-tag">#戦略</li>
-                    <li class="c-tag">#その他</li>
                   </ul>
                 </div>
                 
               </a>
             </article>
-          </div>
+            </li>
 
-        </div>
-      </div>
+             <li class="p-top-new__item">
+         <article class="c-card" aria-labelledby="card-title-1">
+              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link" tabindex="-1">
+                
+                <div class="c-card__img">
+                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304">
+                </div>
+                
+                <div class="c-card__body">
+                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
+                  
+                  <h3 class="c-card__title" id="card-title-1">
+                  <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
+                  </h3>
+                  
+                  <div class="c-card__category">
+                    <span class="c-category-label">TIPS</span>
+                  </div>
+                  
+                  <ul class="p-mv__card-tags" aria-label="記事のタグ">
+                    <li class="c-tag">#コンサルファーム</li>
+                    <li class="c-tag">#戦略</li>
+                  </ul>
+                </div>
+                
+              </a>
+            </article>
+            </li>
 
-      <div class="p-mv__swiper-button-wrap">
-        <div class="c-swiper-nav p-mv__swiper-prev swiper-button-prev" role="button" aria-label="前のスライドへ">
-          <?php include get_theme_file_path("/assets/images/arrow-left.svg"); ?>
+             <li class="p-top-new__item">
+         <article class="c-card" aria-labelledby="card-title-1">
+              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link" tabindex="-1">
+                
+                <div class="c-card__img">
+                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304">
+                </div>
+                
+                <div class="c-card__body">
+                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
+                  
+                  <h3 class="c-card__title" id="card-title-1">
+                    <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
+                  </h3>
+                  
+                  <div class="c-card__category">
+                    <span class="c-category-label">TIPS</span>
+                  </div>
+                  
+                  <ul class="p-mv__card-tags" aria-label="記事のタグ">
+                    <li class="c-tag">#コンサルファーム</li>
+                  </ul>
+                </div>
+                
+              </a>
+            </article>
+            </li>
+        </ul>
+        <div class="p-top-new__button-wrap">
+            <a href="" class="c-button-more">
+                <span class="c-button-more__text">もっと見る</span>
+                <span class="u-sr-only">新着記事一覧へ</span>
+            </a>
         </div>
-        <div class="c-swiper-nav p-mv__swiper-next swiper-button-next" role="button" aria-label="次のスライドへ">
-          <?php include get_theme_file_path("/assets/images/arrow-right.svg"); ?>
-        </div>
-      </div>
-    </div>    
-
-  </div>
+    </div>
 </section>
