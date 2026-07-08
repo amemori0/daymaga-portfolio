5a7957fd01525c73c7a8cf299994bd2d5caa0a15
Amemori <vokey5258fn@gmail.com>
2026-07-08 10:45:35 +0900

    feat: トップページの新着記事セクションをWordPress化
    - section-new.phpを新規実装（WP_Query: 新着順に3件取得）
    - 静的HTMLだった3枚のカードを動的ループに置き換え
    - card.phpを共通利用し、一覧側と同じli構造のまま実装
    - 「もっと見る」ボタンのリンク先を/all/に接続

--- wordpress/themes/daymaga/template-parts/front-page/section-new.php
+++ wordpress/themes/daymaga/template-parts/front-page/section-new.php
@@ -1,104 +1,41 @@
+<?php
+/**
+ * フロントページ: 新着記事セクション
+ * 新着順に3件を表示。
+ */
+$new_query = new WP_Query([
+  "post_type" => "post",
+  "posts_per_page" => 3,
+  "orderby" => "date",
+  "order" => "DESC",
+]); ?>
+
+<?php if ($new_query->have_posts()): ?>
 <section class="p-top-new">
-    <div class="p-top-new__inner">
-        <h2 class="c-section-title p-top-new__title" data-align="center">
-            <svg aria-hidden="true" focusable="false" width="72" height="38" viewBox="0 0 72 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M42.9056 38C30.7853 26.529 -4.20111 4.86719 0.417236 1.75967C13.3482 -6.94112 27.3432 18.4447 42.9056 38Z" fill="#135097"/>
-                <path d="M34.0415 36.01C43.1022 27.4348 75.5692 16.9946 71.6778 14.3764C59.465 6.15945 45.6753 21.3913 34.0415 36.01Z" fill="#C93A3A"/>
-            </svg>
-            <span>新着記事</span>
-        </h2>
-        <ul class="p-top-new__list">
-            <li class="p-top-new__item">
-         <article class="c-card" aria-labelledby="card-title-1">
-              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link">
-                
-                <div class="c-card__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304" loading="lazy">
-                </div>
-                
-                <div class="c-card__body">
-                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
-                  
-                  <h3 class="c-card__title" id="card-title-1">
-                    <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
-                  </h3>
-                  
-                  <div class="c-card__category">
-                    <span class="c-category-label">TIPS</span>
-                  </div>
-                  
-                  <ul class="p-top-new__card-tags" aria-label="記事のタグ">
-                    <li class="c-tag">#コンサルファーム</li>
-                    <li class="c-tag">#戦略</li>
-                  </ul>
-                </div>
-                
-              </a>
-            </article>
-            </li>
+  <div class="p-top-new__inner">
+    <h2 class="c-section-title p-top-new__title" data-align="center">
+      <svg aria-hidden="true" focusable="false" width="72" height="38" viewBox="0 0 72 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M42.9056 38C30.7853 26.529 -4.20111 4.86719 0.417236 1.75967C13.3482 -6.94112 27.3432 18.4447 42.9056 38Z" fill="#135097"/>
+        <path d="M34.0415 36.01C43.1022 27.4348 75.5692 16.9946 71.6778 14.3764C59.465 6.15945 45.6753 21.3913 34.0415 36.01Z" fill="#C93A3A"/>
+      </svg>
+      <span>新着記事</span>
+    </h2>
 
-             <li class="p-top-new__item">
-         <article class="c-card" aria-labelledby="card-title-1">
-              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link">
-                
-                <div class="c-card__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304" loading="lazy">
-                </div>
-                
-                <div class="c-card__body">
-                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
-                  
-                  <h3 class="c-card__title" id="card-title-1">
-                  <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
-                  </h3>
-                  
-                  <div class="c-card__category">
-                    <span class="c-category-label">TIPS</span>
-                  </div>
-                  
-                  <ul class="p-top-new__card-tags" aria-label="記事のタグ">
-                    <li class="c-tag">#コンサルファーム</li>
-                    <li class="c-tag">#戦略</li>
-                  </ul>
-                </div>
-                
-              </a>
-            </article>
-            </li>
+    <ul class="p-top-new__list">
+      <?php while ($new_query->have_posts()):
+        $new_query->the_post(); ?>
+        <?php get_template_part("template-parts/card"); ?>
+      <?php
+      endwhile; ?>
+    </ul>
 
-             <li class="p-top-new__item">
-         <article class="c-card" aria-labelledby="card-title-1">
-              <a href="<?php echo esc_url(home_url("/")); ?>" class="c-card__link">
-                
-                <div class="c-card__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/image_sample_01.webp")); ?>" alt="" width="540" height="304" loading="lazy">
-                </div>
-                
-                <div class="c-card__body">
-                  <time class="c-card__date" datetime="2024-03-20">2024.03.20</time>
-                  
-                  <h3 class="c-card__title" id="card-title-1">
-                    <span class="c-card__title-inner">Big Fourが新たなデジタルイノベーションラボを設立、業界に変革をもたらす</span>
-                  </h3>
-                  
-                  <div class="c-card__category">
-                    <span class="c-category-label">TIPS</span>
-                  </div>
-                  
-                  <ul class="p-top-new__card-tags" aria-label="記事のタグ">
-                    <li class="c-tag">#コンサルファーム</li>
-                  </ul>
-                </div>
-                
-              </a>
-            </article>
-            </li>
-        </ul>
-        <div class="p-top-new__button-wrap">
-            <a href="" class="c-button-more">
-                <span class="c-button-more__text">もっと見る</span>
-                <span class="u-sr-only">新着記事一覧へ</span>
-            </a>
-        </div>
+    <div class="p-top-new__button-wrap">
+      <a href="<?php echo esc_url(home_url("/all/")); ?>" class="c-button-more">
+        <span class="c-button-more__text">もっと見る</span>
+        <span class="u-sr-only">新着記事一覧へ</span>
+      </a>
     </div>
+  </div>
 </section>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
